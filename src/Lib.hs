a3ba587552987b1ff291326165e80b4b91b0e98b
clrnd <welcometothechango@gmail.com>
2016-07-21 16:51:31 -0300

    storing

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,39 +1,54 @@
-{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE TypeOperators #-}
 module Lib
     ( startApp
     ) where
 
-import Data.Aeson
-import Data.Aeson.TH
-import Data.Text
+import Data.Acid
+import Data.ByteString.Char8 (unpack)
+import Data.Text.Encoding (encodeUtf8)
+import Control.Monad.IO.Class
+import Crypto.Hash
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Servant
+import qualified Data.Text as Text
+import qualified Data.Map as Map
 
+
+import Store
 import Debug.Trace
 
-data Snippet = Snippet
-  { snippetContent :: Text
-  } deriving (Eq, Show)
-
-$(deriveJSON defaultOptions ''Snippet)
-
-data SnippetId = SnippetId
-  { snippetId :: Int
-  } deriving (Eq, Show)
-
-$(deriveJSON defaultOptions ''SnippetId)
-
 type API = "store" :> ReqBody '[JSON] Snippet :> Post '[JSON] SnippetId
+      :<|> "get" :> Capture "id" String :> Get '[JSON] Snippet
 
 server :: Server API
-server = snippet
+server = snippet :<|> get
   where
     snippet :: Snippet -> Handler SnippetId
-    snippet s = traceShow s $ return (SnippetId 1)
+    snippet (Snippet s) = do
+        let bytes = encodeUtf8 s
+        let hex = digestToHexByteString (hash bytes :: Digest MD5)
+        let sid = SnippetId . unpack $ hex
+        sid <- liftIO $ do
+            state <- openLocalState (SnippetDb Map.empty)
+            update state (AddSnippet sid $ Snippet s)
+            closeAcidState state
+            return sid
+        return sid
+
+    get :: String -> Handler Snippet
+    get ss = do
+        liftIO $ do
+            state <- openLocalState (SnippetDb Map.empty)
+            snip <- query state (GetSnippet $ SnippetId ss) >>= \case
+                Nothing -> return $ Snippet Text.empty
+                Just s -> return $ s
+            closeAcidState state
+            return snip
 
 api :: Proxy API
 api = Proxy
---
+++ src/Store.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+module Store where
+
+import Control.Monad.Reader (ask)
+import Control.Monad.State (modify)
+import Data.Aeson
+import Data.Aeson.TH
+import Data.Acid
+import Data.Map (Map)
+import Data.SafeCopy
+import Data.Typeable
+import Data.Text (Text)
+import qualified Data.Map as Map
+
+newtype SnippetId = SnippetId
+  { snippetId :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''SnippetId)
+
+newtype Snippet = Snippet
+  { snippetContent :: Text
+  } deriving (Eq, Show, Typeable)
+
+$(deriveJSON defaultOptions ''Snippet)
+
+newtype SnippetDb = SnippetDb
+    { allSnippets :: Map String Snippet
+    } deriving (Typeable)
+
+
+addSnippet :: SnippetId -> Snippet -> Update SnippetDb ()
+addSnippet (SnippetId id') snippet = modify go
+  where
+    go (SnippetDb db) = SnippetDb $
+        Map.insert id' snippet db
+
+getSnippet :: SnippetId -> Query SnippetDb (Maybe Snippet)
+getSnippet (SnippetId id') =
+    Map.lookup id' . allSnippets <$> ask
+
+deriveSafeCopy 0 'base ''Snippet
+deriveSafeCopy 0 'base ''SnippetId
+deriveSafeCopy 0 'base ''SnippetDb
+makeAcidic ''SnippetDb ['getSnippet, 'addSnippet]
