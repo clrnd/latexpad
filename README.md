840e349465a1d182d77d5de7c44d9a8c47522a8c
clrnd <welcometothechango@gmail.com>
2018-07-05 20:54:49 -0300

    swagger docs generator

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ A LaTeX Scratchpad with simple save functionality.
 released under the [GNU AGPL v3](http://www.gnu.org/licenses/agpl.html)
 * Made with [Servant](http://haskell-servant.readthedocs.io/)
 and [acid-state](https://hackage.haskell.org/package/acid-state)
+
+## Instalation
+
+As simple as `stack build && stack install` and then `latexpad-server`.
+
+## Extras
+
+The API is documented with [Swagger](swagger.io). To build the docs
+run `servant-swagger > docs.html` after building and open the resulting HTML file.
+
+It also has a mock version with `latexpad-mock` which will
+return random data.
--- app/Swagger.hs
+++ app/Swagger.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
-import Data.Aeson.Encode.Pretty (encodePretty)
+import NeatInterpolation
+import Data.Aeson.Text (encodeToLazyText)
 import Data.Swagger
 import Servant.Swagger
-import qualified Data.ByteString.Lazy.Char8 as BL8
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy as T
 
 import App
 import Types
@@ -11,5 +15,35 @@ import Types
 instance ToSchema Snippet
 instance ToSchema SnippetId
 
+jsonSwagger :: T.Text
+jsonSwagger = encodeToLazyText $ toSwagger api
+
+interpolate :: Text -> Text
+interpolate json = [text|
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui.css">
+        <script src="https://unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js"></script>
+        <script src="https://unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
+    </head>
+    <body>
+        <div id="swagger-ui">
+        </div>
+        <script>
+        const SPEC = ${json};
+        const ui = SwaggerUIBundle({
+            spec: SPEC,
+            dom_id: '#swagger-ui',
+            presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset
+            ],
+            layout: "StandaloneLayout"
+        })
+        </script>
+    </body>
+    </html>
+|]
+
 main :: IO ()
-main = BL8.putStrLn $ encodePretty $ toSwagger api
+main = T.putStrLn . interpolate . T.toStrict $ jsonSwagger
